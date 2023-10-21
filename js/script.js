6504c931df216cd7a3dbd199655296c3af459aa4
manuellathays13 <147250635+manuellathays13@users.noreply.github.com>
2023-10-21 17:33:23 -0300

    aula 21/10

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// tres constantes para referenciar as ITEM nas DiVS projetos 
+
+const album = document.getElementById('AlbumFotos');
+const site = document.getElementById('Casamentos');
+const port = document.getElementById('Buffet');
+
+const videoAlbum = document.querySelector('videoAlbum');
+const videoCasamentos = document.querySelector('videoCasamentos');
+const videoBuffet = document.querySelector('videoBuffet');
+
+// evento de click
+album.addEventListener('click' ,() =>{
+    form.classList.toggle('active');
+    videoAlbum.classList.toggle(active);
+});
+
+album.addEventListener('click' ,() =>{
+    form.classList.toggle('active');
+    videoCasamentos.classList.toggle(active);
+});
+
+album.addEventListener('click' ,() =>{
+    form.classList.toggle('active');
+    videoBuffet.classList.toggle(active);
+});
+
